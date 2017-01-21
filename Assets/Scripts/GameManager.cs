4e7f68b1937bff0595422a8b2f4a25ee43435397
Robert Broersma <robertbroersma@gmail.com>
2017-01-21 01:46:33 +0100

    Implement Main Screen Scene + Pause Screen script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public float waitUntilFinish;
+
+	private PauseManager pauseManager;
+
+	// Use this for initialization
+	void Start () {
+		// Begin Timer for Finish
+		pauseManager = this.GetComponent<PauseManager>();
+
+		StartCoroutine(Finish(waitUntilFinish));
+	}
+
+	IEnumerator Finish (float waitUntilFinish){
+		yield return new WaitForSeconds (waitUntilFinish);
+
+		pauseManager.OnPause ();
+	}
+
+}

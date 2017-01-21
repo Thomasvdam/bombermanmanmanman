4f92240321f0f41ab3880a878c38d82794841aee
Tamara Roep <t.roep@wearetriple.com>
2017-01-21 10:59:34 +0100

    Working on StartArea script. The purpose is to wait with starting the game untill all characters have moved to the start area.

---
+++ Assets/Scripts/StartAreaScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAreaScript : MonoBehaviour {
+
+	private const int AMOUNT_PLAYERS_IN_AREA_NEEDED = 4;
+	private const string TAG_PLAYER = "Player";
+
+	private int mPlayersInArea = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D co) {
+		Debug.Log ("onTriggerEnter2D");
+		if (co.tag.Equals (TAG_PLAYER)) {
+			mPlayersInArea++;
+			if (mPlayersInArea == AMOUNT_PLAYERS_IN_AREA_NEEDED) {
+				startTimer ();
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D co) {
+		Debug.Log ("onTriggerExit2D");
+		if (co.tag.Equals (TAG_PLAYER)) {
+			mPlayersInArea--;
+		}
+	}
+
+	private void StartTimer() {
+	}
+}

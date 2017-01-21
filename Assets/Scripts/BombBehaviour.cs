ca199494373a697493915eb0e42f6de768b0db34
Thomas van Dam <t.vandam@wearetriple.com>
2017-01-21 14:05:24 +0100

    Add dying and respawning

--- Assets/Scripts/BombBehaviour.cs
+++ Assets/Scripts/BombBehaviour.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombBehaviour : MonoBehaviour {
+public class BombBehaviour : MonoBehaviour, IFallable {
 
 	public GameObject shockwave;
 
-	public float waitTillExplode;
+	public float waitTillExplode = 3;
+	public float fallingSpeed = 1;
+	public float fallingDuration = 2;
+
+	private bool isFalling = false;
+
+	private Rigidbody2D rBody;
 
 	// Use this for initialization
 	void Start () {
 		// Begin Timer for explosion
 		StartCoroutine(Explode(waitTillExplode));
+		rBody = GetComponent<Rigidbody2D> ();
 	}
 
 	void Update () {
-		if (!transform.parent) {
-			return;
+		if (isFalling) {
+			transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.1f, 0.1f, 0.1f), Time.deltaTime * fallingSpeed);
 		}
 
 		transform.position = transform.parent.position + transform.parent.up * 1f; 
@@ -25,8 +32,22 @@ public class BombBehaviour : MonoBehaviour {
 	IEnumerator Explode (float waitTillExplode){
 		yield return new WaitForSeconds (waitTillExplode);
 		//Instantiate shockwave when time is over
-		Instantiate (shockwave, transform.position, Quaternion.identity);
-		Destroy (this.gameObject);
+		if (gameObject) {
+			Instantiate (shockwave, transform.position, Quaternion.identity);
+			Destroy (this.gameObject);	
+		}
 	}
 
+	public void Fall () {
+		isFalling = true;
+		rBody.velocity = rBody.velocity / 2;
+		rBody.drag = 1;
+
+		StartCoroutine(Kill(fallingDuration));
+	}
+
+	IEnumerator Kill (float timer){
+		yield return new WaitForSeconds (timer);
+		Destroy (this.gameObject);
+	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, IFallable {
 
 	public float speed = 1;
+	public float fallingSpeed = 1;
+	public float fallingDuration = 2;
 
 	private PlayerActions actions;
 	private Rigidbody2D rBody;
+	private bool isFalling = false;
+
+	public delegate void EventHandler(GameObject e, int id);
+	public event EventHandler onPlayerDeath;
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +24,18 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isFalling) {
+			transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.1f, 0.1f, 0.1f), Time.deltaTime * fallingSpeed);
+		}
 	}
 
 	public void Move(float horizontal, float vertical) {
+		if (isFalling) {
+			return;
+		}
+
 		Vector2 direction = new Vector2 (horizontal, vertical);
-
 		transform.up = direction;
-
 		rBody.AddForce (direction * speed);
 	}
 
@@ -34,5 +45,20 @@ public class Player : MonoBehaviour {
 
 	public void ThrowBomb(float x, float y) {
 		actions.ThrowBomb (new Vector2(x, y));
+	}
+
+	public void Fall () {
+		isFalling = true;
+		rBody.velocity = Vector2.zero;
+		StartCoroutine(Kill(fallingDuration));
+	}
+
+	IEnumerator Kill (float timer){
+		yield return new WaitForSeconds (timer);
+
+		Destroy (gameObject);
+		if (onPlayerDeath != null) {
+			onPlayerDeath (gameObject, 1);
+		}
 	}
 }
